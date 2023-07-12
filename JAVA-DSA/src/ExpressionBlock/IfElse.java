85a202f1d9bfc4bdab0123279a4f0f2f37b7d7bc
princekumarpaswan <prasantandal123@gmail.com>
2023-07-12 12:57:29 +0530

    added new files

---
+++ JAVA-DSA/src/ExpressionBlock/IfElse.java
@@ -0,0 +1,17 @@
+package ExpressionBlock;
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+//        created an object of Scanner class
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an intiger value : ");
+
+        int number = input.nextInt();
+
+        System.out.println("Your input is :" + number);
+
+    }
+}
